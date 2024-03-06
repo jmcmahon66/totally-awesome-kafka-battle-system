b4c2a76eef1af496bf6b63bbfdf1a8b52fa7daba
jmcmahon <j.a.mcmahon66@gmail.com>
2024-03-06 17:00:35 +0000

    added fixture for tests setup

--- test_battle.py
+++ test_battle.py
@@ -4,19 +4,10 @@ from helper import *
 from main import Battle
 from weapons import create_weapons
 import random
+import pytest
 
-# lower_damage = 5
-# upper_damager = 15
-
-# def randomise_damage():
-#     return random.randint(lower_damage, upper_damager)
-
-# def test_randomise_damage():
-#     for i in range(20):
-#         random_damage = randomise_damage()
-#         assert lower_damage <= random_damage <= upper_damage
-
-def test_character_attack():
+@pytest.fixture
+def characters_and_weapons():
     weapons = create_weapons()
     heroWeapon = random.choice(weapons)
     enemyWeapon = random.choice(weapons)
@@ -25,36 +16,28 @@ def test_character_attack():
 
     hero = Character(name="Hero", health=startingHealth, weapon=heroWeapon)
     enemy = Character(name="Enemy", health=startingHealth, weapon=enemyWeapon)
+
+    return hero, enemy, startingHealth
+
+def test_character_attack(characters_and_weapons):
+    hero, enemy, startingHealth = characters_and_weapons
     
     # Testing the attack method
     hero.attack(enemy)
     # Assert that enemy has taken damage
     assert enemy.health < startingHealth  # enemy health should decrease by hero's damage
 
-def test_character_health_limit():
-    weapons = create_weapons()
-    heroWeapon = random.choice(weapons)
-    enemyWeapon = random.choice(weapons)
-
-    startingHealth = 100
-
-    hero = Character(name="Hero", health=startingHealth, weapon=heroWeapon)
-    enemy = Character(name="Enemy", health=2, weapon=enemyWeapon)
+def test_character_health_limit(characters_and_weapons):
+    hero, enemy, startingHealth = characters_and_weapons
     
     # Testing if health cannot go below 0
+    enemy.health = 2
     hero.attack(enemy)
     assert enemy.health == 0  # enemy health should not go below 0
 
-def test_battle():
-    weapons = create_weapons()
-    heroWeapon = random.choice(weapons)
-    enemyWeapon = random.choice(weapons)
+def test_battle(characters_and_weapons):
+    hero, enemy, startingHealth = characters_and_weapons
 
-    startingHealth = 100
-
-    hero = Character(name="Hero", health=startingHealth, weapon=heroWeapon) 
-    enemy = Character(name="Enemy", health=startingHealth, weapon=enemyWeapon)
-    
     # Test battle function
     battle = Battle(hero, enemy)
     battle.run()
